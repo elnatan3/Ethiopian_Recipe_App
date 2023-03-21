041447d01eb431766a516754c243cb29efa10946
elnatan3 <107220803+elnatan3@users.noreply.github.com>
2023-03-21 00:35:32 -0500

    Updated the Recipe for instructions para

--- RecipeApp/src/Recipe.java
+++ RecipeApp/src/Recipe.java
@@ -2,39 +2,58 @@ class Recipe {
 private String name;
 private String description;
 private boolean isFavorite;
-public Recipe(String name, String description) {
-    this.name = name;
-    this.description = description;
-    this.isFavorite = false;
-}
+private String instructions;
+	public Recipe(String name, String description, String instructions) {
+	    this.name = name;
+	    this.description = description;
+	    this.isFavorite = false;
+	    this.instructions = instructions;
+	}
 
-public String getName() {
-    return name;
-}
+	public String getName() {
+	    return name;
+	}
 
-public String getDescription() {
-    return description;
-}
+	public String getDescription() {
+	    return description;
+	}
 
 
-public void editRecipe(String name, String description) {
-	// TODO Auto-generated method stub
-	this.name = name;
-	this.description = description;
-	
-}
+	public void editRecipe(String name, String description, String instruction) {
+		// TODO Auto-generated method stub
+		this.name = name;
+		this.description = description;
+		this.instructions = instruction;
 
-public boolean isFavorite() {
-	return isFavorite;
-}
+	}
 
-public void setFavorite(boolean favorite) {
-	isFavorite = favorite;
-}
+	public boolean isFavorite() {
+		return isFavorite;
+	}
 
-public void setDescription(String text) {
-	// TODO Auto-generated method stub
-	description = text;
-	
-}
+	public void setFavorite(boolean favorite) {
+		isFavorite = favorite;
+	}
+
+	public void setDescription(String text) {
+		// TODO Auto-generated method stub
+		this.description = text;
+	}
+	public String getInstructions() {
+	    return instructions;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setInstructions(String instruction) {
+		this.instructions = instruction;
+		// TODO Auto-generated method stub
+
+	}
 }
