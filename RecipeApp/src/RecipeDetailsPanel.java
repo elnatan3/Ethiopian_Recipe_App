197b69d5c361f7d8d6d3d8085a09b883c8916bc8
elnatan3 <107220803+elnatan3@users.noreply.github.com>
2023-03-21 00:36:23 -0500

    updated the GUI for the text areas

--- RecipeApp/src/RecipeDetailsPanel.java
+++ RecipeApp/src/RecipeDetailsPanel.java
@@ -9,28 +9,50 @@ public class RecipeDetailsPanel extends JPanel {
 
     private JTextArea descriptionArea;
     private JCheckBox favoriteCheckBox;
+    private JLabel nameLabel;
+    private JTextArea instructionArea;
 
     public RecipeDetailsPanel() {
-        setLayout(new BorderLayout());
+    	setLayout(new BorderLayout());
+
+        nameLabel = new JLabel();
+        nameLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        add(nameLabel, BorderLayout.NORTH);
 
         JPanel fieldsPanel = new JPanel(new GridLayout(0, 1));
         descriptionArea = new JTextArea();
         descriptionArea.setLineWrap(true);
         descriptionArea.setWrapStyleWord(true);
-        JScrollPane scrollPane = new JScrollPane(descriptionArea);
-        scrollPane.setPreferredSize(new Dimension(200, 100));
+        descriptionArea.setEditable(false);
+        JScrollPane descriptionScrollPane = new JScrollPane(descriptionArea);
+        
+        
+        instructionArea = new JTextArea();
+        instructionArea.setEditable(false);
+        instructionArea.setLineWrap(true);
+        instructionArea.setWrapStyleWord(true);
+        JScrollPane instructionsScrollPane = new JScrollPane(instructionArea);
+
+        
+        
+    
         favoriteCheckBox = new JCheckBox("Favorite");
         favoriteCheckBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                currentRecipe.setFavorite(favoriteCheckBox.isSelected());
+            	if (currentRecipe != null) {
+            		currentRecipe.setFavorite(favoriteCheckBox.isSelected());
+            
+            	}
             }
         });
         fieldsPanel.add(new JLabel("Description:"));
-        fieldsPanel.add(scrollPane);
-        fieldsPanel.add(favoriteCheckBox);
+        fieldsPanel.add(descriptionScrollPane);
+        fieldsPanel.add(new JLabel("Instructions:"));
+        fieldsPanel.add(instructionsScrollPane);
+        
         add(fieldsPanel, BorderLayout.CENTER);
-
+        fieldsPanel.add(favoriteCheckBox);
         JButton saveButton = new JButton("Save");
         saveButton.addActionListener(new ActionListener() {
             @Override
@@ -49,9 +71,13 @@ public class RecipeDetailsPanel extends JPanel {
         currentRecipe = recipe;
         if (recipe != null) {
             descriptionArea.setText(recipe.getDescription());
+            nameLabel.setText(recipe.getName());
+            descriptionArea.setText(recipe.getDescription());
+            instructionArea.setText(recipe.getInstructions());
             favoriteCheckBox.setSelected(recipe.isFavorite());
         } else {
             descriptionArea.setText("");
+            instructionArea.setText("");
             favoriteCheckBox.setSelected(false);
         }
     }
